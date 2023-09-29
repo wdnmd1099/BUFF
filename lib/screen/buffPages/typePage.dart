42e9c34c7a6767a4fcb102858e67e1ee61780b69
ggbond <wdnmd@gmail.com>
2023-09-29 09:45:06 +0800

    更新保存8，修复当用户点击不限后，想再选取其它选项时跳提示。

--- lib/screen/buffPages/typePage.dart
+++ lib/screen/buffPages/typePage.dart
@@ -29,6 +29,7 @@ class _State extends State<TypePage> {
   List<String> test(String item,List<String> middleList,String title){
     // 检查当前点击元素是否已经存在于数组
     bool isItemInArray = middleList.contains(item);
+    List allNone = [];
 
     if(item == '不限'){
       // 如果点击的是不限，就把该类目的所有已选择元素从selected中删除
@@ -39,10 +40,7 @@ class _State extends State<TypePage> {
           }
         }
       }
-
-
-      List allNone = [];
-      // 如果
+      // 如果title已被添加，就什么都不做，如果没有添加，则要看列表是否全部都没有添加过，全部没有才添加
       for(int x=0;x<selected.length;x++) {
         if(selected[x] == title){
 
@@ -50,15 +48,11 @@ class _State extends State<TypePage> {
           allNone.add(false);
         }
       }
-
-      if(allNone.length == selected.length){
-        selected.add(title);
-      }
       middleList = ['不限'];
     }
 
 
-    //若已存在，删除
+    //若当前点击元素已存在，删除
     if(isItemInArray){
       middleList.remove(item);
       selected.remove(item);
@@ -67,7 +61,7 @@ class _State extends State<TypePage> {
         selected.remove(title);
       }
     }
-    //若不存在
+    //若当前点击元素不存在
     else if(selected.length < 5){
       if(item != '不限'){
         //如果点击的元素不是不限，就检查元素里有没有不限，有就删除
@@ -85,13 +79,25 @@ class _State extends State<TypePage> {
         //然后添加点击的元素
         middleList.add(item);
         selected.add(item);
+      }else if(item == '不限'){
+        //把点击不限后添加总类目的逻辑放到这里，如果放到上面的不限类目，会先添加再判断是否为5个，这样第五个选择不限时会预期外地跳出提示
+        if(allNone.length == selected.length){
+          selected.add(title);
+        }
+      }
+    }else if(selected.length == 5){
+      //如果此时selected的第5个是总类目的话，说明用户需要点击其它选项，放弃不限，所以需要删除不限添加用户点击的选项
+      //这样可以修复当用户点击不限后，想再选取其它选项时跳提示。
+      if(selected[4] == title){
+        selected.remove(selected[4]);
+        middleList = [];
+        middleList.add(item);
+        selected.add(item);
+      }else{
+        BotToast.showText(text:"最多只能选择五项",textStyle: TextStyle(fontSize: 12,color: Colors.white));
       }
 
-
-    }else if(selected.length == 5){
-      BotToast.showText(text:"最多只能选择五项",textStyle: TextStyle(fontSize: 12,color: Colors.white));
     }
-
     return middleList;
   }
 
